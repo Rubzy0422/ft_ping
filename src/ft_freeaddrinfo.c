4768d209f79cbac6899bd21d8b523bfc5cde6d21
rubzy0422 <rcoetzer5@gmail.com>
2020-10-07 09:08:13 +0200

    rDNS added

--- src/ft_freeaddrinfo.c
+++ src/ft_freeaddrinfo.c
@@ -6,7 +6,7 @@
 /*   By: rcoetzer <rcoetzer@student.wethinkcode.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/06 13:22:44 by rcoetzer          #+#    #+#             */
-/*   Updated: 2020/10/06 14:53:10 by rcoetzer         ###   ########.fr       */
+/*   Updated: 2020/10/07 09:07:13 by rcoetzer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ void ft_freeaddrinfo(struct addrinfo *aihead)
 	ai = aihead;
 	while (ai != NULL) {
 		if (ai->ai_addr != NULL)
-			free(ai->ai_addr);		/* socket address structure */
+			free(ai->ai_addr);
 
 		if (ai->ai_canonname != NULL)
 			free(ai->ai_canonname);
 
-		ainext = ai->ai_next;	/* can't fetch ai_next after free() */
-		free(ai);				/* the addrinfo{} itself */
+		ainext = ai->ai_next;
+		free(ai);
 		ai = ainext;
 	}
 }
